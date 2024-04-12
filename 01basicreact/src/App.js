75230565a4e5da7e2e2e01d195fa6efcdd845e72
Shubham <bhattacharya.shubham123@gmail.com>
2024-04-12 17:48:24 -0500

    creating app two different ways

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello World | learned about pakage.json</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello vite + react | learned about vite.config.js 사용법</h1>
+  )
+}
+
+export default App
